f06acddbbd739b0ed6f51c6b90a6d7aa6e948181
Cleberson <cleberson.ricardo@hotmail.com>
2025-01-06 12:00:00 +0000

    Add Day_1 project files

---
+++ Day_1/bonus1.py
@@ -0,0 +1,3 @@
+text = input("Enter a title: ")
+length = len(text)
+print("The text has ", length, " caracthers")
---
+++ Day_1/main.py
@@ -0,0 +1,8 @@
+usert_prompt = 'Enter a todo:'
+todo1 = input(usert_prompt)
+todo2 = input(usert_prompt)
+todo3 = input(usert_prompt)
+
+todos = [todo1,todo2,todo3]
+
+print(todos)
