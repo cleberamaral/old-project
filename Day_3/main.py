9af0f395ae0fe1efa41e506f5281e33b13024122
Cleberson <cleberson.ricardo@hotmail.com>
2025-01-23 12:00:00 +0000

    Add Day_3 project files

--- Day_3/main.py
+++ Day_3/main.py
@@ -2,11 +2,11 @@ todos = []
 
 while True:
     user_action = input("Type add or show, or exit: ")
-
     match user_action.strip():
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo.capitalize())
+
         case 'show':
             for item in todos:
                 print(item)
@@ -14,4 +14,5 @@ while True:
             break
         case _:
             print("Hey, you have entered a wrong option")
+
 print ("Bye")
